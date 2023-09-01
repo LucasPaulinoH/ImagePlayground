35badda51623d89469ece95ac6c8442411eda6a6
Lucas Paulino <lucashsp12@hotmail.com>
2023-09-01 11:00:09 -0300

    FEAT: adding most of high-pass filters

--- src/utils/SecondUnityOperations/HighPassFilters.ts
+++ src/utils/SecondUnityOperations/HighPassFilters.ts
@@ -2,28 +2,29 @@ import { HighPassFilters } from "../../types/filters";
 
 export const executeHighPassFilter = (
   image: HTMLCanvasElement,
-  highPassFilterType: HighPassFilters
+  highPassFilterType: HighPassFilters,
+  factor?: number
 ): HTMLCanvasElement => {
   let resultingImageCanvas = null;
 
   switch (highPassFilterType) {
     case HighPassFilters.H1:
-      resultingImageCanvas = h1(image);
+      resultingImageCanvas = highPassFilter(image, h1Mask);
       break;
     case HighPassFilters.H2:
-      resultingImageCanvas = h2(image);
+      resultingImageCanvas = highPassFilter(image, h2Mask);
       break;
     case HighPassFilters.M1:
-      resultingImageCanvas = m1(image);
+      resultingImageCanvas = highPassFilter(image, m1Mask);
       break;
     case HighPassFilters.M2:
-      resultingImageCanvas = m2(image);
+      resultingImageCanvas = highPassFilter(image, m2Mask);
       break;
     case HighPassFilters.M3:
-      resultingImageCanvas = m3(image);
+      resultingImageCanvas = highPassFilter(image, m3Mask);
       break;
     case HighPassFilters.HIGH_BOOST:
-      resultingImageCanvas = highBoost(image);
+      resultingImageCanvas = highBoost(image, factor ?? 1);
       break;
     default:
       break;
@@ -32,9 +33,89 @@ export const executeHighPassFilter = (
   return resultingImageCanvas;
 };
 
-const h1 = (image: HTMLCanvasElement): HTMLCanvasElement => {};
-const h2 = (image: HTMLCanvasElement): HTMLCanvasElement => {};
-const m1 = (image: HTMLCanvasElement): HTMLCanvasElement => {};
-const m2 = (image: HTMLCanvasElement): HTMLCanvasElement => {};
-const m3 = (image: HTMLCanvasElement): HTMLCanvasElement => {};
-const highBoost = (image: HTMLCanvasElement): HTMLCanvasElement => {};
+const h1Mask = [
+  [0, -1, 0],
+  [-1, 4, -1],
+  [0, -1, 0],
+];
+
+const h2Mask = [
+  [-1, -1, -1],
+  [-1, 8, -1],
+  [-1, -1, -1],
+];
+
+const m1Mask = [
+  [-1, -1, -1],
+  [-1, 9, -1],
+  [-1, -1, -1],
+];
+
+const m2Mask = [
+  [1, -2, 1],
+  [-2, 5, -2],
+  [1, -2, 1],
+];
+
+const m3Mask = [
+  [0, -1, 0],
+  [-1, 5, -1],
+  [0, -1, 0],
+];
+
+const highPassFilter = (
+  image: HTMLCanvasElement,
+  mask: number[][]
+): HTMLCanvasElement => {
+  const ctxIn = image.getContext("2d");
+  const canvasOut = document.createElement("canvas");
+  const ctxOut = canvasOut.getContext("2d");
+
+  if (!ctxIn || !ctxOut) {
+    throw new Error("Não foi possível obter os contextos 2D dos canvas.");
+  }
+
+  canvasOut.width = image.width;
+  canvasOut.height = image.height;
+
+  const imageDataIn = ctxIn.getImageData(0, 0, image.width, image.height);
+  const dataIn = imageDataIn.data;
+
+  for (let y = 0; y < image.height; y++) {
+    for (let x = 0; x < image.width; x++) {
+      let r = 0,
+        g = 0,
+        b = 0;
+
+      for (let j = -1; j <= 1; j++) {
+        for (let i = -1; i <= 1; i++) {
+          const pixelX = x + i;
+          const pixelY = y + j;
+
+          if (
+            pixelX >= 0 &&
+            pixelX < image.width &&
+            pixelY >= 0 &&
+            pixelY < image.height
+          ) {
+            const index = (pixelY * image.width + pixelX) * 4;
+            const maskValue = mask[j + 1][i + 1];
+
+            r += dataIn[index] * maskValue;
+            g += dataIn[index + 1] * maskValue;
+            b += dataIn[index + 2] * maskValue;
+          }
+        }
+      }
+
+      ctxOut.fillStyle = `rgba(${r}, ${g}, ${b}, 255)`;
+      ctxOut.fillRect(x, y, 1, 1);
+    }
+  }
+
+  return canvasOut;
+};
+
+const highBoost = (image: HTMLCanvasElement, amplificationFactor: number): HTMLCanvasElement => {
+  
+};
